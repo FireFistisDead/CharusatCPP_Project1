1795688dc4c85de1bfa19fb2258deba59896ce67
VedantPanchal23 <148608437+VedantPanchal23@users.noreply.github.com>
2024-04-15 12:38:25 +0530

    Update by 23AIML042

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 #include <vector>
-// #include <cstdlib>
+#include <cstdlib>
 #include <ctime>
 using namespace std;
 
 // Function to display the Tic Tac Toe board
-void displayBoard(const vector<vector<char>>& board) {
-    for (int i = 0; i < 3; ++i) {
+void displayBoard(const vector<vector<char>> &board)
+{
+    /*for (int i = 0; i < 3; ++i) {
             // cout << "-";
         // cout<< "| ";
         for (int j = 0; j < 3; ++j) {
             cout <<board[i][j] << "|" <<" ";
         }
         cout << endl;
+    }*/
+    cout << "  1   2   3\n";
+
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << i + 1 << " ";
+        for (int j = 0; j < 3; ++j)
+        {
+            cout << board[i][j];
+            if (j < 2)
+                cout << " | ";
+        }
+        cout << endl;
+        if (i < 2)
+            cout << "  ---------\n";
     }
 }
 
